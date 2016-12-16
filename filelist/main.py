6296f012b75313fd50e0d7b262a0b977cfe5e5a3
Valentin Olaru <vali@pidginhost.com>
2016-12-16 20:32:29 +0200

    update

--- filelist/main.py
+++ filelist/main.py
@@ -28,40 +28,43 @@ class Base(TorrentProvider):
 
             try:
                 result_table = html.find('div', attrs={'class': 'visitedlinks'})
-                if not result_table or 'Nu s-a gasit nimic!' in data.lower():
-                    return False
+                if 'Nu s-a gasit nimic!' not in data.lower() or result_table:
 
-                entries = result_table.find_all('div', attrs={'class': 'torrentrow'})
-                for result in entries[0:]:
+                    entries = result_table.find_all('div', attrs={'class': 'torrentrow'})
+                    for result in entries[0:]:
 
-                    all_cells = result.find_all('div')
+                        all_cells = result.find_all('div')
 
-                    torrent = all_cells[1].find('a')
-                    download = all_cells[3].find('a')
+                        torrent = all_cells[1].find('a')
+                        download = all_cells[3].find('a')
 
-                    torrent_id = torrent['href']
-                    torrent_id = torrent_id.replace('details.php?id=', '')
+                        torrent_id = torrent['href']
+                        torrent_id = torrent_id.replace('details.php?id=', '')
 
-                    torrent_name = torrent.getText()
+                        torrent_name = torrent.getText()
 
-                    torrent_size = self.parseSize(all_cells[6].getText())
-                    torrent_seeders = tryInt(all_cells[8].getText())
-                    torrent_leechers = tryInt(all_cells[9].getText())
-                    torrent_url = self.urls['baseurl'] % download['href']
-                    torrent_detail_url = self.urls['baseurl'] % torrent['href']
+                        torrent_size = self.parseSize(all_cells[6].getText())
+                        torrent_seeders = tryInt(all_cells[8].getText())
+                        torrent_leechers = tryInt(all_cells[9].getText())
+                        torrent_url = self.urls['baseurl'] % download['href']
+                        torrent_detail_url = self.urls['baseurl'] % torrent['href']
 
-                    results.append({
-                        'id': torrent_id,
-                        'name': torrent_name,
-                        'size': torrent_size,
-                        'seeders': torrent_seeders,
-                        'leechers': torrent_leechers,
-                        'url': torrent_url,
-                        'detail_url': torrent_detail_url,
-                    })
+                        results.append({
+                            'id': torrent_id,
+                            'name': torrent_name,
+                            'size': torrent_size,
+                            'seeders': torrent_seeders,
+                            'leechers': torrent_leechers,
+                            'url': torrent_url,
+                            'detail_url': torrent_detail_url,
+                        })
+                else:
+                    log.debug('No search results found.')
 
             except Exception as e:
                 log.error('Failed getting results from {}: {}'.format(self.getName(), e))
+        else:
+            log.debug('No search results found.')
 
     def getLoginParams(self):
         log.info('Logging in to filelist.ro with user [{}]'.format(self.conf('username')))
